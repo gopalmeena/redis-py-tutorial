c68a52750f5b4c63eb577031ee1e7b88b0f0df38
Gopal Singh Meena <gopalmeena94@gmail.com>
2016-12-27 01:10:54 +0530

    First command with incomplete data

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,32 @@
 var redisApp = angular.module('redisApp',['ngMaterial']);
+redisApp.controller('RedisCtrl',function($scope,$mdDialog){
+	//write your code here
+	$scope.showTabDialog = function(ev) {
+		$mdDialog.show({
+			controller: DialogController,
+			templateUrl: 'expire.html',
+			parent: angular.element(document.body),
+			targetEvent: ev,
+			clickOutsideToClose:true
+		})
+		.then(function(answer) {
+			$scope.status = 'You said the information was "' + answer + '".';
+		}, function() {
+			$scope.status = 'You cancelled the dialog.';
+		});
+	};
+
+	function DialogController($scope, $mdDialog) {
+		$scope.hide = function() {
+			$mdDialog.hide();
+		};
+
+		$scope.cancel = function() {
+			$mdDialog.cancel();
+		};
+
+		$scope.answer = function(answer) {
+			$mdDialog.hide(answer);
+		};
+	}
+});
